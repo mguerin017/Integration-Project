71b1c5f707b329903cb4f6826818077e72e877f8
mguerin017 <54415455+mguerin017@users.noreply.github.com>
2019-09-16 00:15:11 -0400

    Add files via upload
    Fixed welcome message
    Added arithmetic quiz

--- Main.py
+++ Main.py
@@ -1,2 +1,37 @@
 # Mark Guerin
-print(Welcome!)
+print("Welcome to this short quiz.")
+print("Type two numbers whose sum is 7.")
+num1 = int(input("Type first number: "))
+num2 = int(input("Type second number: "))
+if num1 + num2 == 7:
+    print("Correct!")
+
+elif num1 + num2 != 7:
+    print("Incorrect")
+
+print("Type two numbers whose difference is 4.")
+num1 = int(input("Type first number: "))
+num2 = int(input("Type second number: "))
+if num1 - num2 == 4:
+    print("Correct!")
+
+elif num1 - num2 != 4:
+    print("Incorrect")
+
+print("Type two numbers whose product is 18.")
+num1 = int(input("Type first number: "))
+num2 = int(input("Type second number: "))
+if num1 * num2 == 18:
+    print("Correct!")
+
+elif num1 * num2 != 18:
+    print("Incorrect")
+
+print("Type two numbers whose quotient is 4.")
+num1 = int(input("Type first number: "))
+num2 = int(input("Type second number: "))
+if num1 / num2 == 4:
+    print("Correct!")
+
+elif num1 / num2 != 4:
+    print("Incorrect")
